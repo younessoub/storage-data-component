1d2f3320088da40da7f6f14e34b84bf925ddb0a0
Youness <oubelkacem.youness@gmail.com>
2023-03-15 16:53:17 +0100

    first commit

---
+++ src/components/Storage/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Storage.css";
+
+function Storage() {
+  return (
+    <div className="Storage">
+      <div className="Storage-header"></div>
+      <div className="Storage-stats"></div>
+      StorageYou’ve used 815 GB of your storage 185 GB Left 0 GB 1000 GB jjdjdjd
+    </div>
+  );
+}
+
+export default Storage;
